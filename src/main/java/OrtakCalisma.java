15e565ece3554816a70baf6de3e53b8028f6af1d
Sevimsmgr <sevimsmgr@gmail.com>
2023-01-04 18:36:13 +0100

    V2

--- src/main/java/OrtakCalisma.java
+++ src/main/java/OrtakCalisma.java
@@ -1,2 +1,11 @@
 public class OrtakCalisma {
+
+    public static void main(String[] args) {
+
+
+
+
+        String x = " Calm down !";
+
+    }
 }
--- src/main/java/Sevim.java
+++ src/main/java/Sevim.java
@@ -2,5 +2,8 @@ public class Sevim {
 
     public static void main(String[] args) {
         System.out.println("gun 1 : deneme 1");
+
+
+        System.out.println("2.deneme");
     }
 }
